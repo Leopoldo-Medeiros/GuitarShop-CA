e7f839a4b361f5e5b36b915f22eeda1add1eabcd
Leopoldo Medeiros <leopoldof.medeiros@gmail.com>
2020-04-25 14:02:49 +0000

    heroku test

---
+++ routes.js
@@ -0,0 +1,12 @@
+var express = require('express'),
+router = express.Router(),
+userCtrl = require('./user-controller');
+
+router.get('/', (req, res) => res.send('Hello World!'));
+router.post('/users', userCtrl.createUser);
+router.get('/users', userCtrl.getUsers);
+router.get('/users/:id', userCtrl.getUser);
+router.delete('/users/:id', userCtrl.deleteUser);
+router.put('/users/:id', userCtrl.updateUser);
+
+module.exports = router;
